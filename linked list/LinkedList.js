6401a28143899bdd4d4a020901f8c4104b9a3bd3
ichater <chaterizaak@gmail.com>
2020-08-23 11:58:19 +1000

    remove at head and remove at index

--- linked list/LinkedList.js
+++ linked list/LinkedList.js
@@ -24,6 +24,11 @@ class LinkedList {
     return current;
   }
 
+  removeHead() {
+    this.head = this.head.next;
+    this.length--;
+  }
+
   insertAtIndex(index, value) {
     if (index === 0) return this.insertAtHead(value);
 
@@ -32,6 +37,16 @@ class LinkedList {
 
     prev.next = new LinkedListNode(value, prev.next);
     this.length++;
+  }
+
+  removeAtIndex(index) {
+    if (index === 0) return this.removeHead();
+
+    const prev = this.getByIndex(index - 1);
+    if (prev == null) return null;
+
+    prev.next = prev.next.next;
+    this.length--;
   }
 
   print() {
