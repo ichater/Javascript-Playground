1b9a047e2affa73118f469444e3889fe7ae2fd28
ichater <chaterizaak@gmail.com>
2020-09-07 08:42:13 +1000

    object destructuring exercise from khent c dodds article

--- DestructuringObjects.js
+++ DestructuringObjects.js
@@ -1,32 +1,85 @@
-let person = {
-  firstName: "John",
-  age: 28,
-  lastName: "Doe",
+// let person = {
+//   firstName: "John",
+//   age: 28,
+//   lastName: "Doe",
+// };
+// let person2 = {
+//   firstName: "Jane",
+//   age: 24,
+//   lastName: "Doey",
+//   address: {
+//     city: "somewhere else",
+//     state: "Another one",
+//   },
+// };
+
+// const {
+//   firstName: name,
+//   age,
+//   favouriteFood = "ricecream",
+//   address: { city, state },
+// } = person2;
+
+// console.log(name);
+// console.log(age);
+// console.log(favouriteFood);
+// console.log(city);
+// console.log(state);
+
+// //overwrite shared attibutesd of "person2" with "person"
+// const person3 = { ...person2, ...person };
+
+// console.log(person3);
+
+function nestedArrayAndObject() {
+  // refactor this to a single line of destructuring...
+  const info = {
+    title: "Once Upon a Time",
+    protagonist: {
+      name: "Emma Swan",
+      enemies: [
+        { name: "Regina Mills", title: "Evil Queen" },
+        { name: "Cora Mills", title: "Queen of Hearts" },
+        { name: "Peter Pan", title: `The boy who wouldn't grow up` },
+        { name: "Zelena", title: "The Wicked Witch" },
+      ],
+    },
+  };
+  const {
+    title,
+    protagonist: {
+      name,
+      enemies: [{ name: enemyName, title: enemyTitle }],
+    },
+  } = info; // <-- replace the next few `const` lines with this
+
+  return `${enemyName} (${enemyTitle}) is an enemy to ${name} in "${title}"`;
+}
+
+console.log(nestedArrayAndObject());
+
+const metadata = {
+  title: "Scratchpad",
+  translations: [
+    {
+      locale: "de",
+      localization_tags: [],
+      last_edit: "2014-04-14T08:43:37",
+      url: "/de/docs/Tools/Scratchpad",
+      title: "JavaScript-Umgebung",
+    },
+  ],
+  url: "/en-US/docs/Tools/Scratchpad",
 };
-let person2 = {
-  firstName: "Jane",
-  age: 24,
-  lastName: "Doey",
-  address: {
-    city: "somewhere else",
-    state: "Another one",
-  },
-};
 
-const {
-  firstName: name,
-  age,
-  favouriteFood = "ricecream",
-  address: { city, state },
-} = person2;
+let {
+  title: englishTitle, // rename
+  translations: [
+    {
+      title: localeTitle, // rename
+    },
+  ],
+} = metadata;
 
-console.log(name);
-console.log(age);
-console.log(favouriteFood);
-console.log(city);
-console.log(state);
-
-//overwrite shared attibutesd of "person2" with "person"
-const person3 = { ...person2, ...person };
-
-console.log(person3);
+console.log(englishTitle); // "Scratchpad"
+console.log(localeTitle);
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Javascript-Playground
-A home made javascript "cheat sheet" where I keep myself up to date with the basic and advanced concepts of javascript
+
+A home made javascript "cheat sheet" where I keep myself up to date with the basic and advanced concepts of javascript. There are a lot of things that I don't use in my regular projects like classes and the "this" keyword by extension. So I've put this together just to follow along with some articles to ensure that I have at least some knowledge of the parts of javascript I don't use as often.
