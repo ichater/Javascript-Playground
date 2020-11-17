5bd36fc13923d866116bbfe4470de7362e954ecd
ichater <chaterizaak@gmail.com>
2020-11-17 18:17:44 +1100

    still trying to get the for loop terenary to work

--- src/ConnectFourPlayground.js
+++ src/ConnectFourPlayground.js
@@ -96,23 +96,23 @@ const board = [
     { token: null },
   ],
   [
-    { token: "red" },
-    { token: "red" },
-    { token: "red" },
+    // { token: "red" },
+    // { token: "red" },
+    // { token: "red" },
     { token: null },
     { token: null },
     { token: null },
   ],
   [
-    { token: "red" },
-    { token: "yellow" },
-    { token: "yellow" },
+    // { token: "red" },
+    // { token: "yellow" },
+    // { token: "yellow" },
     { token: null },
     { token: null },
     { token: null },
   ],
   [
-    { token: "red" },
+    // { token: "red" },
     { token: null },
     { token: null },
     { token: null },
@@ -120,7 +120,7 @@ const board = [
     { token: null },
   ],
   [
-    { token: "red" },
+    // { token: "red" },
     { token: null },
     { token: null },
     { token: null },
@@ -145,11 +145,11 @@ const board = [
   ],
 ];
 
-const i = 0;
+// const i = 0;
 
-const result = [board[i][0], board[i + 1][0], board[i + 2][0], board[i + 3][0]];
+// const result = [board[i][0], board[i + 1][0], board[i + 2][0], board[i + 3][0]];
 
-console.table(board);
+// console.table(board);
 
 // const isConnectedHorizontal = (gameCells, i, j) => {
 //   const result = gameCells[i].slice(gameCells[i][j], 4);
@@ -192,20 +192,15 @@ console.table(board);
 
 // console.log(isConnectedVerticle(board));
 
-// const arr = [
-//   { token: "red" },
-//   { token: "yellow" },
-//   { token: "yellow" },
-//   { token: null },
-//   { token: null },
-//   { token: null },
-//   { token: "red" },
-//   { token: "yellow" },
-//   { token: "yellow" },
-//   { token: null },
-//   { token: null },
-//   { token: null },
-// ];
+for (let i = 0; i < board.length; i++) {
+  for (let j = 0; j < board[i].length; j++) {
+    if (board[i][j].token !== null) {
+      break;
+    }
 
-// const j = 2;
-// console.log(arr.slice(j, j + 4));
+    let length = board.length - 1;
+    return length === i && board[length].length - 1 === j
+      ? console.log("false")
+      : console.log("true");
+  }
+}
