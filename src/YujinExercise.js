4c7992d54e18152c59c0910cf502dc3399ea698b
ichater <chaterizaak@gmail.com>
2021-06-27 13:55:49 +1000

    Yujin exercises

---
+++ src/YujinExercise.js
@@ -0,0 +1,50 @@
+// const bill = 275;
+// let tip = bill <= 300 && bill >= 50 ?  bill * .15:  bill * .2
+
+// console.log(6000*14)
+// console.log("the bill was " + bill + ", the tip was " + tip)
+// console.log(`the bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+// if(bill !== 1000){
+//     if(bill === 500){
+
+//     }else {
+
+// }
+
+// }
+
+// function fruitProcessor(pears, bruno) {
+//     // console.log(apples, oranges);
+//     const milo = `Juice with ${pears} apples and ${bruno} oranges.`;
+//     return milo;
+// }
+
+// const teach = fruitProcessor(8,5)
+// console.log(teach)
+// console.log(fruitProcessor(8,5))
+
+// function fruitProcessor(apples, oranges) {
+//     // console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);// 5, 0  are arguments
+// console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+// Yujin Ok
+// Yujin sen t Today at 7:06 PM
+
+const dolphinsOne = [44, 23, 71];
+const dolphinsTwo = [85, 54, 41];
+const koalasOne = [65, 54, 49];
+const koalasTne = [23, 34, 27];
+
+const calcAverage = (scoreArray) =>
+  scoreArray.reduce((acc, cur) => acc + cur) / scoreArray.length + 1;
+
+const checkWinner = (teamOne, teamTwo) => {
+  const winner = (winner, loser) => ``;
+};
